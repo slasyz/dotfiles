71d89351a48c5c92460c8198078ca033981b0eb0
Vyacheslav Syrovatsky <slasyz@gmail.com>
2022-07-17 23:59:32 +0300

    Add subl, nvim and dotfiles aliases.

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -6,5 +6,10 @@ alias ls='ls -F -G '
 alias lss='ls -Flh -G '
 alias l='exa -Flh'
 
+alias subl='"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"'
+alias vim='nvim'
+
 #alias c=__conda_setup="$('/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"; eval "$__conda_setup"; unset __conda_setup; PATH=$PATH:/opt/anaconda3/bin
 #alias cml="c && conda activate ml"
+
+alias dotfiles='cd ~/.dotfiles && git pull && cd -'
