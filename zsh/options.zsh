8ec014e14cb5ce85d56d5da339600dc00f14a4e8
Vyacheslav Syrovatsky <slasyz@gmail.com>
2022-12-14 22:57:06 +0700

    Small fixes fixes.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -11,19 +11,21 @@ setopt share_history          # share command history data
 
 # ohmyzsh-style history search:
 # Typing ls and hitting Up Arrow will scroll through commands that start with what you typed:
-# matching `ls`` and `ls -l`` but not `echo ls`.
+# matching `ls` and `ls -l` but not `echo ls`.
 # start typing + [Up-Arrow] - fuzzy find history forward
 if [[ "${terminfo[kcuu1]}" != "" ]]; then
     autoload -U up-line-or-beginning-search
     zle -N up-line-or-beginning-search
-    bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+    bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search  # TODO: why this doesn't always work?
 fi
+bindkey "^[[A" up-line-or-beginning-search
 # start typing + [Down-Arrow] - fuzzy find history backward
 if [[ "${terminfo[kcud1]}" != "" ]]; then
     autoload -U down-line-or-beginning-search
     zle -N down-line-or-beginning-search
     bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
+bindkey "^[[B" down-line-or-beginning-search
 
 
 # cd without cd
